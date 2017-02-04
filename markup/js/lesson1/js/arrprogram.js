a5394bc469affab3f70cf8d248dc870316d1c8d2
Tania Maslivets <t.mykhailiuk1@gmail.com>
2017-02-04 16:28:55 +0200

    js hw2

--- markup/js/lesson1/js/arrprogram.js
+++ markup/js/lesson1/js/arrprogram.js
@@ -1,27 +1,27 @@
 "use strict";
 
 var arr = [];
+var user = null;
 
 for (var i = 0; i < 5; i++) {
     var a = prompt("Введите имя");
-    if (a == null || a == ""){
+    if (a == null || a == "") {
         alert("Вы не ввели имя");
         break;
-    } else{
-    arr.push(a);
-    console.log(arr);
+    } else {
+        arr.push(a);
     }
 }
-function askUserName(){
-    if (arr.length < 5){
+function askUserName() {
+    if (arr.length < 5) {
         return false;
     }
-var user = prompt("ВВедите имя пользователя");
+    user = prompt("ВВедите имя пользователя");
 }
 
 // есть ли пользователь в массиве arr
 
-function isNameInArr (arr, user){
+function isNameInArr(arr, user) {
     for (var j = 0; j < arr.length; j++) {
         if (user !== arr[j]) {
             continue;
@@ -32,13 +32,15 @@ function isNameInArr (arr, user){
 
     return false;
 }
+
+askUserName();
 var result = isNameInArr(arr, user);
 
-if (result == true){
+if (result == true) {
     alert(user + ", вы успешно вошли");
-}else if(arr.length < 5){
+} else if (arr.length < 5) {
     alert("Попробуйте еще раз")
-} else{
+} else {
     alert("Пользователя нет в массиве");
 }
 
