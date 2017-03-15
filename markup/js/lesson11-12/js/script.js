72107623605c3aff2922ae7d562ebd8b257546c7
Tania <t.mykhailiuk1@gmail.com>
2017-03-15 11:34:09 +0200

    hw11-12

--- markup/js/lesson11-12/js/script.js
+++ markup/js/lesson11-12/js/script.js
@@ -2,6 +2,48 @@
 
 $(function(){
 
-$('body').carousel();
+//$('body').carousel();
+
+    var html = $('#test').html();
+
+    var data = {
+        name: 'Масливец Татьяна',
+        className: [
+            'content',
+            'personal_info',
+            'my_photo',
+            'study_goals',
+            'contact_info',
+            'tel_number',
+            'fb',
+            'fb_profile',
+            'feedback'
+        ],
+        url: 'img/My_photo.jpg',
+        position: 'Аккаунт-менеджер в Grape Ukraine',
+        goal:'Хочу учить фронтенд, потому что:',
+        items: [
+            {
+            content: 'Делать что-то своими руками - это удовольствие'
+            },
+            {
+            content:'Можно сразу увидеть результат своей работы'
+            },
+            {
+            content:'Необходимо всегда изучать что-то новое'
+            }
+        ],
+        phone: 'Мой контактный телефон',
+        number: '+380631870183',
+        fb: 'Мой профиль в Facebook:',
+        fbLink:'https://www.facebook.com/t.mykhailiuk',
+        fbText: 'facebook.com',
+        feedback: 'Мой фидбек',
+        feedbacText: 'Знаю, когда у клиента бывает 100500 правок )'
+    };
+
+    var content = tmpl(html, data);
+
+    $('body').append(content);
 
 });
