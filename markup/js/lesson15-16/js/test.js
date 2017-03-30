c2a8851b6fd322b41696a4aaae8fafb36d30d234
Tania Maslivets <t.mykhailiuk1@gmail.com>
2017-03-30 09:52:34 +0300

    hw 15-16

---
+++ markup/js/lesson15-16/js/test.js
@@ -0,0 +1,9 @@
+'use strict';
+
+class Test{
+    constructor(question, answer,rightAnswer){
+        this.question = question;
+        this.answer = answer;
+        this.rightAnswer = rightAnswer;
+    }
+}
