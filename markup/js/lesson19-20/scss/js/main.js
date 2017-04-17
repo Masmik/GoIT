018a13b1471e3f802a34269e91321fefaca048c9
Tania Maslivets <t.mykhailiuk1@gmail.com>
2017-04-17 18:31:30 +0300

    hw19-20 add new feature to accordion

--- markup/js/lesson19-20/scss/js/main.js
+++ markup/js/lesson19-20/scss/js/main.js
@@ -1,5 +1,19 @@
 $(function () {
-    $("#accordion").accordion();
+    $("#accordion").accordion({
+        collapsible: true
+    });
+    $("#accordion1").accordion({
+        collapsible: true,
+        active: false
+    });
+    $("#accordion2").accordion({
+        collapsible: true,
+        active: false
+    });
+    $("#accordion3").accordion({
+        collapsible: true,
+        active: false
+    });
     $('.my-slider').unslider({
         arrows: false,
         autoplay: true
