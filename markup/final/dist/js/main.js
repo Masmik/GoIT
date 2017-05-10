f2da221943e9aa6863b50f587cccfb63ecb15f06
Tania Maslivets <t.mykhailiuk1@gmail.com>
2017-05-10 20:44:33 +0300

    gulp

---
+++ markup/final/dist/js/main.js
@@ -0,0 +1,166 @@
+'use strict';
+// document.addEventListener("DOMContentLoaded", onReady);
+
+function domReady(fn, context) {
+
+    function onReady(event) {
+        document.removeEventListener("DOMContentLoaded", onReady);
+        fn.call(context, event);
+    }
+
+    function onReadyIe(event) {
+        if (document.readyState === "complete") {
+            document.detachEvent("onreadystatechange", onReadyIe);
+            fn.call(context, event);
+        }
+    }
+
+    document.addEventListener && document.addEventListener("DOMContentLoaded", onReady) ||
+    document.attachEvent && document.attachEvent("onreadystatechange", onReadyIe);
+}
+
+function onPageReady() {
+    loadSliders();
+    loadMasonry();
+}
+
+domReady(onPageReady);
+
+'use strict';
+
+function loadMasonry() {
+
+    var grid = document.querySelector('.grid');
+    new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        gutter: '.gutter-sizer',
+    });
+
+    var searchWords = ['cat', 'dog', 'pigs', 'sun', 'smile', 'egypt', 'sea', 'sky', 'tree', 'sand', 'space'];
+
+    var searchWordIndex = Math.floor(Math.random() * (searchWords.length - 1));
+    search(searchWords[searchWordIndex]);
+    search_partner();
+
+    function masonry_build(data) {
+        var gridItems = document.getElementsByClassName('grid-item');
+        for (var i = 0; i < gridItems.length; i++) {
+            gridItems[i].style.backgroundImage = 'url(' + ((data.hits)[i].webformatURL) + ')';
+            var itemText = gridItems[i].getElementsByClassName('item__text')[0];
+            itemText.innerHTML = data.hits[i].tags.toUpperCase().split(',')[0];
+        }
+    }
+
+    function search(request) {
+
+        jQuery.support.cors = true;
+
+        $.ajax({
+            url: 'https://pixabay.com/api/?key=5270170-c6e51883c4d17561ecf30e5fa&q=' + request + '&image_type=photo&min_width=260&per_page=7',
+            dataType: 'json',
+            method: 'GET',
+            success: function (data) {
+                if (data.count == 0) {
+                    console.log('pixabay не вернул ни одной картинки');
+                    return;
+                }
+                else {
+                    masonry_build(data);
+                }
+            },
+            fail: function () {
+                console.log('Fail request');
+            },
+            done: function () {
+                console.log('Done request');
+            }
+        });
+
+
+        // var xmlhttp = new XMLHttpRequest();
+        //
+        // xmlhttp.onreadystatechange = function () {
+        //     if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+        //         if (xmlhttp.status == 200) {
+        //             var response = xmlhttp.responseText;
+        //             var responseJson = JSON.parse(response);
+        //             if (responseJson.length == 0) {
+        //                 console.log('pixabay не вернул ни одной картинки');
+        //                 return;
+        //             }
+        //             else {
+        //                 masonry_build(responseJson);
+        //             }
+        //         }
+        //         else if (xmlhttp.status == 400) {
+        //             console.log('There was an error 400');
+        //         }
+        //         else
+        //             console.log('something else other than 200 was returned');
+        //         }
+        //     }
+        // };
+        //
+        // xmlhttp.open('GET', 'https://pixabay.com/api/?key=5270170-c6e51883c4d17561ecf30e5fa&q=' + request + '&image_type=photo&min_width=260&per_page=7', true);
+        // xmlhttp.send();
+    }
+
+    function search_partner() {
+        var searchPartnerButtonEl = document.getElementsByClassName('search-partners_button')[0];
+        searchPartnerButtonEl.addEventListener('click', function (e) {
+            e.preventDefault();
+            var searchQuery = document.getElementById('search_query');
+            search(searchQuery.value);
+        });
+    }
+
+}
+
+var App = App || {};
+
+App.MasSlider = function (sliderContainer) {
+
+    var sliderIndex = 0;
+    var sliders = sliderContainer.getElementsByClassName('slider');
+    var prev = sliderContainer.getElementsByClassName('prev')[0];
+    var next = sliderContainer.getElementsByClassName('next')[0];
+
+    prev.addEventListener('click', prevSlide);
+    next.addEventListener('click', nextSlide);
+
+    renderSlider();
+
+    function prevSlide() {
+        if (sliderIndex == 0) {
+            sliderIndex = sliders.length;
+        }
+        sliderIndex--;
+        renderSlider();
+    }
+
+    function nextSlide() {
+        if (sliderIndex == sliders.length - 1) {
+            sliderIndex = -1;
+        }
+        sliderIndex++;
+        renderSlider();
+    }
+
+    function renderSlider() {
+        for (var i = 0; i < sliders.length; i++) {
+            sliders[i].style.display = 'none';
+        }
+        sliders[sliderIndex].style.display = 'block';
+    }
+
+};
+
+
+function loadSliders() {
+    var sliderContainer = document.getElementsByClassName("slider-container");
+
+    for (var i = 0; i < sliderContainer.length; i++) {
+        new App.MasSlider(sliderContainer[i]);
+    }
+}
--- markup/final/gulpfile.js
+++ markup/final/gulpfile.js
@@ -3,24 +3,57 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var imagemin = require('gulp-imagemin');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var uglify = require('gulp-uglify');
+var spritesmith = require('gulp.spritesmith');
 
 gulp.task('sass', function(){
-    return gulp.src('src/css/main.scss')
-        .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    return gulp.src('src/css/main*.scss')
+        .pipe(sass())
         .pipe(gulp.dest('dist/css'))
 });
 
 
 gulp.task('img', function () {
-    return gulp.src("src/img/*")
+    return gulp.src('src/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('concat', function() {
+    return gulp.src('src/js/*.js')
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('dist/css/*.css')
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('uglify-js', function () {
+    return gulp.src(['dist/js/*.js', 'src/js/polifill-and-masonry/*.js'])
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/uglify'))
+});
+
+gulp.task('sprite', function generateSpritesheets () {
+    var spriteData = gulp.src('src/img/partner-icons/*.png')
+        .pipe(spritesmith({
+            imgName: 'spritesheet.png',
+            cssName: 'sprites.scss'
+        }));
+    spriteData.img.pipe(gulp.dest('src/img'));
+    spriteData.css.pipe(gulp.dest('src/css'));
 });
 
 
 gulp.task('watch', function () {
     gulp.watch("src/css//*.scss", ['sass']);
     gulp.watch("src/img/*", ['img']);
+    gulp.watch("src/js/*", ['js']);
 });
 
-gulp.task("default", ['img', 'saas', 'watch']);
+gulp.task("default", ['img', 'sass', 'concat','minify-css','uglify-js','sprite','watch']);
